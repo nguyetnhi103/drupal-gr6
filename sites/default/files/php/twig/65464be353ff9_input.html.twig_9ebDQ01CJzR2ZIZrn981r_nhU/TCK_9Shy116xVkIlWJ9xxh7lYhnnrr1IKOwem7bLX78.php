aab1e44b7dac8b4d46fced062749c91202fe9e35
nguyenlinh080507@gmail.com <nguyenlinh080507@gmail.com>
2023-11-12 16:33:50 +0700

    new file

---
+++ sites/default/files/php/twig/65464be353ff9_input.html.twig_9ebDQ01CJzR2ZIZrn981r_nhU/TCK_9Shy116xVkIlWJ9xxh7lYhnnrr1IKOwem7bLX78.php
@@ -0,0 +1,108 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/revolt/templates/form/input.html.twig */
+class __TwigTemplate_0c46811837b315905da7d0ef90ecea1a extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 13
+        echo "
+";
+        // line 14
+        if ((twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "type", [], "any", false, false, true, 14) == "submit")) {
+            // line 15
+            echo "    <input";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "button--primary"], "method", false, false, true, 15), "removeClass", [0 => "form-text"], "method", false, false, true, 15), 15, $this->source), "html", null, true);
+            echo " />";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null), 15, $this->source), "html", null, true);
+            echo "
+";
+        } else {
+            // line 17
+            echo "    <input";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "form-control"], "method", false, false, true, 17), "removeClass", [0 => "form-text"], "method", false, false, true, 17), 17, $this->source), "html", null, true);
+            echo " />";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null), 17, $this->source), "html", null, true);
+            echo "
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/revolt/templates/form/input.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  52 => 17,  44 => 15,  42 => 14,  39 => 13,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/revolt/templates/form/input.html.twig", "C:\\xampp\\htdocs\\drupal-gr6-1\\themes\\contrib\\revolt\\templates\\form\\input.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 14);
+        static $filters = array("escape" => 15);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
